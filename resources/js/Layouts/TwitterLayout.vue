405feff01089af8a4296dd1a4b28c605a374303a
Roberto Sasso <rsasso25@gmail.com>
2023-11-15 15:43:11 +0100

    end base layout x

---
+++ resources/js/Layouts/TwitterLayout.vue
@@ -0,0 +1,112 @@
+<script setup>
+import { ref } from 'vue';
+import { router } from '@inertiajs/vue3';
+import Twitter from 'vue-material-design-icons/Twitter.vue';
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import Close from 'vue-material-design-icons/Close.vue';
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
+import Earth from 'vue-material-design-icons/Magnify.vue';
+import Imageoutline from 'vue-material-design-icons/Imageoutline.vue';
+import FileGifBox from 'vue-material-design-icons/FileGifBox.vue';
+import Emoticon from 'vue-material-design-icons/Emoticon.vue';
+import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue';
+import MenuItem from '@/Components/MenuItem.vue';
+
+</script>
+
+<template>
+    <div class="fixed w-full">
+        <div class="max-w-[1400px] flex mx-auto">
+            <div class="lg:w-3/12 w-[60px] h-[100vh] max-w-[350px] lg:px-4 lg:mx-auto">
+                <div class="p-2 px-3 mb-4">
+                    <img src="/image/x_logo.png" class="logo-x">
+                </div>
+                <MenuItem iconString="Home" />
+                <MenuItem iconString="Explore" />
+                <MenuItem iconString="Notifications" />
+                <MenuItem iconString="Messages" />
+                <MenuItem iconString="Profile" />
+
+                <button
+                    class="
+                        lg:w-full
+                        mt-8
+                        ml-2
+                        text-white
+                        font-extrabold
+                        text=[22px]
+                        bg-[#1C9CEF]
+                        p-3
+                        px-3
+                        rounded-full
+                        cursor-pointer
+                    "
+                >
+                    <span class="lg:block hidden">Tweet</span>
+                    <span class="block lg:hidden"><Plus /></span>
+                </button>
+            </div>
+            <div class="lgt:w-7/12 w-11/12 border-x border-gray-800 relative">
+                <div class="bg-black bg-opacity-50 backdrop-blur-md z-10 absolute w-full">
+                    <div class="border-gray-800 border-b w-full">
+                        <div class="w-full text-white text-[22px] font-extrabold p-4">
+                            Home
+                        </div>
+                        <div class="flex">
+                            <div class="
+                                    flex 
+                                    items-center 
+                                    justify-center 
+                                    w-full 
+                                    h-[60px] 
+                                    text-white 
+                                    text[17px] 
+                                    font-extrabold 
+                                    p-4 
+                                    hover:bg-gray-500
+                                    cursor-pointer
+                                    transition
+                                    duration-200
+                                    ease-in-out
+                                "
+                            >
+                                <div class="inline-block text-center border-b-4 border-b-[#1C9CEF] h-[60px]">
+                                    <div class="my-auto mt-4">Per te</div>
+                                </div>
+                            </div>
+                            <div class="
+                                    w-full 
+                                    h-[60px] 
+                                    text-gray-500 
+                                    text[17px] 
+                                    font-extrabold 
+                                    p-4 
+                                    hover:bg-gray-500
+                                    cursor-pointer
+                                    transition
+                                    duration-200
+                                    ease-in-out
+                                "
+                            >
+                                <div class="text-center">Seguiti</div>                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>1
+        </div>
+    </div>
+</template>
+ 
+<style>
+    body {
+        background-color: black;
+    }
+
+    .logo-x{
+        height: 37px;
+        width: 37px;
+    }
+</style>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import TwitterLayout from '@/Layouts/TwitterLayout.vue';
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+   <TwitterLayout>
+        <div class="bg-red-900">Welcome</div>
+   </TwitterLayout>
+</template>
+
+
